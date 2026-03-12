3221b72333f8503f2c779beaf71877d7a7c496d3
Daniel Sebastian Angulo <danielangulo@unimayor.edu.co>
2026-03-11 21:53:16 -0500

    se agrego DTO con estudiantes y empleos.

---
+++ src/main/java/unimayor/Undertake/UndertakeApplication.java
@@ -0,0 +1,13 @@
+package unimayor.Undertake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndertakeApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(UndertakeApplication.class, args);
+	}
+
+}
